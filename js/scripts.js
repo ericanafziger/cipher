9cc8cdbced7f95edebcd49f0bd1e5b818e868919
Diego Suarez and Erica Nafziger <erica@>
2016-10-17 14:02:42 -0700

    cat pictures and jQuery up to date

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-var sentence = prompt("Enter your sentence to be coded here:");
+  $("#img2").click(function(){
+    alert($("#sentence").val());
+  })
 
-var firstLetter = sentence.charAt(0);
-var lastLetter = sentence.charAt((sentence.length)-1);
-var fl=firstLetter.toUpperCase()+lastLetter.toUpperCase();
-function reverse (input) {
-  return input.split("").reverse().join("");
-}
-function countLetters (input) {
-  return sentence.charAt((Math.floor(sentence.length / 2)));
-}
-alert(reverse(countLetters(sentence)+ sentence+reverse(fl)));
+  $( "#target" ).submit(function( event ) {
+
+      function reverse (input) {
+        return input.split("").reverse().join("");
+      }
+      function countLetters (input) {
+        return sentence.charAt((Math.floor(sentence.length / 2)));
+      }
+
+    var sentence=$("#sentence").val();
+    var firstLetter = sentence.charAt(0);
+    var lastLetter = sentence.charAt((sentence.length)-1);
+    var fl=firstLetter.toUpperCase()+lastLetter.toUpperCase();
+    alert(reverse(countLetters(sentence)+ sentence+reverse(fl)));
+    event.preventDefault();
+  });
+
 });
